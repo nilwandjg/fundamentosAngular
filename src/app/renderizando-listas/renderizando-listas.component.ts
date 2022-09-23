fa21293c326d8ce9a1e513779ee698de325ed7bd
nilwandjg <nilwandjg@yahoo.com.br>
2022-09-23 04:31:37 -0300

    criação do componente renderizando-listas

--- src/app/renderizando-listas/renderizando-listas.component.ts
+++ src/app/renderizando-listas/renderizando-listas.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Celular } from '../types/celular';
 
 @Component({
   selector: 'app-renderizando-listas',
   templateUrl: './renderizando-listas.component.html',
   styleUrls: ['./renderizando-listas.component.css']
 })
-export class RenderizandoListasComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class RenderizandoListasComponent {
+  celulares: Celular[] = [
+    {id: 1, nome: "Celular XL", descricao: "Um celular grande", esgotado: false},
+    {id: 2, nome: "Celular Normal", esgotado: false},
+    {id: 3, nome: "Celular Mini", descricao: "Um celular pequeno", esgotado: true},
+  ]
+    
 }
